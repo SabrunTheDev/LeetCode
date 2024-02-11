f23d0a17f2fb348fbba27cb44d26b18c6fd2826c
Sabrun <officialsabrun@gmail.com>
2024-02-11 02:48:41 -0500

    Two Sum

---
+++ twosum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// Declare a function called twoSum that takes an array of numbers and a target sum
+let nums = [1, 2, 3, 4, 5];
+let target = 9;
+
+var twoSum = function (nums, target) {
+  // Create an empty object to store the index of each number in the array
+  const numIncides = {};
+
+  // Use a for loop to iterate through the array of numbers
+  for (let i = 0; i < nums.length; i++) {
+    // Create a variable (num) to retrive the current num from the array
+    const num = nums[i];
+    // Create a variable to check the difference between the target and the current number (num)
+    const result = target - num;
+
+    // Use an if statement to check if the differece (result) is already in the numIndices object
+    if (result in numIncides) {
+      // Return the index of the result and the current index
+      return [numIncides[result], i];
+    }
+    // If the difference (result) is not in the numIndices, store the current number's index and restart the for loop from the current index
+    numIncides[num] = i;
+  }
+  // If no pairs within the table are found equaling to the target, return null
+  return null;
+};
+
+console.log(twoSum(nums, target));
